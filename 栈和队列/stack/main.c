374517c596d9b443a26c404cb2b430c8b503f7c3
yhinan <1300204051@qq.com>
2018-06-06 19:11:42 +0800

    update main.c

--- 栈和队列/stack/main.c
+++ 栈和队列/stack/main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 #define STACK_INIT_SIZE 100 //栈初始化分配空间
 #define STACKINCREMENT 10   // 存储空间分配增量
 #define OK 1
@@ -20,6 +21,8 @@ Status GetTop(SqStack *stack,SElemType *e);
 Status Push(SqStack *stack, SElemType *e);
 //若栈不为空则删除栈顶元素，用e返回其值，并返回OK，否则返回ERROR
 Status Pop(SqStack *stack, SElemType *e);
+//判断栈是否为空
+Status StackEmpty(SqStack stack);
 Status InitStack(SqStack *stack)
 {
     stack->base = (SElemType * )malloc(STACK_INIT_SIZE * sizeof(SElemType));
@@ -44,8 +47,8 @@ Status Push(SqStack *stack, SElemType *e){
     return OK;
 
 }
-Status StackEmpty(SqStack *stack){
-    if(stack->base == stack->top){
+Status StackEmpty(SqStack stack){
+    if(stack.base == stack.top){
         return OK;
     }else{
         return ERROR;
@@ -57,11 +60,17 @@ Status Pop(SqStack *stack, SElemType *e){
     *e =  *(--stack->top);
     return OK;
 }
-void conversion(){
+
+int GetLength(SqStack stack){
+    int len = (stack.top - stack.base);
+    return len;
+}
+void Decimal_To_Octal(){ //十进制转八进制
     //对于输入的任意一个非负十进制整数，打印输出其等值的八进制数
     SqStack stack;
-    int number,e=2,mod;
+    int number,e,mod;
     InitStack(&stack);
+    printf("输入一个非负十进制整数");
     scanf("%d",&number);
     while (number)
     {   mod = number % 8;
@@ -69,14 +78,35 @@ void conversion(){
         number = number/8;
     }
 
-    while (!StackEmpty(&stack))
+    while (!StackEmpty(stack))
     {
         Pop(&stack,&e);
         printf("%d",e);
     }
 }
+void Binary_To_Decimal(){ //二进制转十进制
+    SqStack stack;
+    InitStack(&stack);
+    char c;
+    int intc,e,sum=0;
+    printf("请输入二进制数,#结束 ");
+    scanf("%c",&c);
+    while(c != '#'){
+        intc = c - '0'; //把char类型字符串转换成int类型
+        Push(&stack,&intc);
+        scanf("%c",&c);
+    }
+    getchar();
+    printf("栈的容量大小：%d",GetLength(stack));
+    for(int i=0; !StackEmpty(stack); i++){
+        Pop(&stack,&e);
+        sum += e*pow(2,i);
+    }
+    printf("十进制数是：%d",sum);
+
+}
 int main()
 {
-    conversion();
+    Binary_To_Decimal();
     return 0;
 }
